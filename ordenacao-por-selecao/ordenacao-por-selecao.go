bf59d9df430f640aa2c3ff69738a3e001050353c
MarioLuiz <marioluiz.arruda@gmail.com>
2023-10-10 09:06:02 -0400

    feat: algoritmo ordenacao por selecao

---
+++ ordenacao-por-selecao/ordenacao-por-selecao.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	listaDesordenada := []int{22, 64, 1, 35, 15, 21, 66, 37, 87, 182, 222, 133, 122, 177, 358, 154, 321, 187, 98, 123}
+	listaOrdenada := []int{}
+
+	for len(listaDesordenada) > 0 {
+		IndiceComMenorValorEncontrado := buscaIndiceComMenorValor(listaDesordenada)
+		// Adicionando menor valor em um novo Slice
+		listaOrdenada = append(listaOrdenada, listaDesordenada[IndiceComMenorValorEncontrado])
+		// Removendo indice do Slice desordenado
+		listaDesordenada = append(listaDesordenada[:IndiceComMenorValorEncontrado], listaDesordenada[IndiceComMenorValorEncontrado+1:]...)
+	}
+	fmt.Println("Lista Desordenada: ", listaDesordenada)
+	fmt.Print("\n")
+	fmt.Println("ListaOrdenada", listaOrdenada)
+}
+
+func buscaIndiceComMenorValor(lista []int) int {
+	menor := lista[0]
+	menorIndice := 0
+	for i := 0; i < len(lista); i++ {
+		if lista[i] < menor {
+			menor = lista[i]
+			menorIndice = i
+		}
+	}
+	return menorIndice
+}
